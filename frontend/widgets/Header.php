4565ed501db50bb8e68a82493aa874ed81699748
javokhirergashev <iqtidorziyosi@gmail.com>
2023-05-01 14:37:18 +0500

    fix

--- frontend/widgets/Header.php
+++ frontend/widgets/Header.php
@@ -2,12 +2,14 @@
 
 namespace frontend\widgets;
 
+use common\models\Menu;
 use yii\bootstrap5\Widget;
 
 class Header extends Widget
 {
     public function run()
     {
-        return $this->render('header');
+        $models = Menu::find()->orderBy(["order_by"=> SORT_ASC])->all();
+        return $this->render('header', compact('models'));
     }
 }
